4cc7ce50e2291a82627983c7072229da131ac147
LucasSouusa <72485990+LucasSouusa@users.noreply.github.com>
2021-10-05 19:36:14 -0300

    Add files via upload

---
+++ Node.js
@@ -0,0 +1,15 @@
+const DataDoEvento=2021;
+   
+   if (DataDoEvento == 2021){
+    console.log("Data permitida");
+   }else{
+    console.log("Data invalida");  
+   }
+
+const Idade=18;
+
+   if(Idade=>18){
+    console.log("cadastro permitido")  
+   }else{
+    console.log("Idade não permitida")  
+   }
