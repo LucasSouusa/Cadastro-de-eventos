59eaf74f27f028103106248a82c7ea4b837ac78b
LucasSouusa <72485990+LucasSouusa@users.noreply.github.com>
2021-10-07 16:51:04 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+const DataEvento=2021;
+
+if (DataEvento == 2021){
+    console.log("Data permitida:",DataEvento)
+}else{
+    console.log("Data não permitida:",DataEvento)
+}
+
+const idade=22;
+
+if (idade => 18){
+    console.log("Cadastro permitido:",idade,"anos");
+}else{
+    console.log("Cadastro não permitido:",idade);
+}
+
+const participantes=90;
+
+if(participantes <=100){
+    console.log("Quantidade permitida:",participantes,"participantes");
+}else{
+    console.log("Quantidade excedida:",participantes,"participantes");
+}
